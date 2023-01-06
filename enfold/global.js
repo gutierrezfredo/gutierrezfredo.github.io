0770a89e49c215356a78053ed6ad05d6808e32c2
Alfredo Gutiérrez <gutierrez.fredo@gmail.com>
2023-01-06 03:12:13 +0000

    global.js

--- enfold/global.js
+++ enfold/global.js
@@ -3,6 +3,6 @@
 jQuery(window).load(function(){
 	jQuery('.open-popup-link a').magnificPopup({
 	  type:'inline',
-	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	  midClick: true
 	});
 });
